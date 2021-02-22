d91a63c8b8225607111a2e6ea7e001ca13f413e7
danbulnet <daniel@bulanda.net>
2021-02-22 21:37:21 +0100

    fix deps

--- notebook/drivingevents.jl
+++ notebook/drivingevents.jl
@@ -10,10 +10,6 @@ begin
 	rootdir = joinpath(@__DIR__, "..")
 	Pkg.activate(rootdir)
 	Pkg.instantiate()
-	Pkg.add("Gadfly")
-	Pkg.precompile()
-	Pkg.add("StatsPlots")
-	Pkg.precompile()
 	cd(rootdir)
 	md"project root and current directory: $(pwd())"
 end
