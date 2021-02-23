c5a6c85847caea4969a168c2c2e1bb41db964cc0
danbulnet <daniel@bulanda.net>
2021-02-23 16:46:41 +0100

    fix rendering bug in static html notebook

--- routes.jl
+++ routes.jl
@@ -4,3 +4,11 @@ using Genie.Renderer.Json
 route("/notebook") do
 	serve_static_file("drivingevents.html")
 end
+
+route("/train") do
+	serve_static_file("drivingevents.html")
+end
+
+route("/predict") do
+	serve_static_file("drivingevents.html")
+end
